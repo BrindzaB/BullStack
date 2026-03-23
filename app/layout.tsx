ff59c8e040b4eabc82f1b9c2fa979911237d8a75
BrindzaB <63659791+BrindzaB@users.noreply.github.com>
2026-03-23 17:18:19 +0100

    blue bends background

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import ReactQueryProvider from "@/components/providers/ReactQueryProvider"
-import Silk from "@/components/ui/Silk"
+import ColorBends from "@/components/ui/ColorBends"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,7 +33,7 @@ export default function RootLayout({
         style={{ position: 'relative' }}
       >
         <div style={{ position: 'fixed', inset: 0, zIndex: -1 }}>
-          <Silk />
+          <ColorBends colors={['#020c18', '#064673', '#0e7490', '#1e1b4b']} />
         </div>
         <ReactQueryProvider>
           {children}
